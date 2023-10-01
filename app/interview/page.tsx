655bee4b8620303266dc69393ed8c22edab8896d
brendanmclaughlin <bmclaughlin0407@gmail.com>
2023-10-01 15:39:46 -0700

    bug with sending audio to backend

--- app/interview/page.tsx
+++ app/interview/page.tsx
@@ -1,6 +1,8 @@
 // interview/page.tsx
 'use client';
 import React, { useEffect, useState } from 'react';
+import { ReactMediaRecorder } from 'react-media-recorder';
+import axios from 'axios';
 
 const InterviewPage: React.FC = () => {
     const [data, setData] = useState({ message: '' });
@@ -16,32 +18,40 @@ const InterviewPage: React.FC = () => {
             setData(data);
         } catch (error) {
             console.error(error);
-        }      
-    }
-
-    // Send the current input value to the server and get AI's response
-    async function handleSendMessage() {
-        try {
-            setMessages([...messages, `You: ${inputValue}`]);
-            setIsLoading(true); // set loading state to true
-
-            const response = await fetch(`http://127.0.0.1:5000/chat`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ text: inputValue })
-            });
-
-            const responseData = await response.json();
-            setMessages([...messages, `You: ${inputValue}`, `AI: ${responseData.response}`]);
-            setIsLoading(false); // reset loading state to false
-            setInputValue('');
-        } catch (error) {
-            console.error(error);
-            setIsLoading(false); // reset loading state to false in case of error
         }
     }
+
+    const handleStopRecording = async (mediaBlobUrl: string | undefined) => {
+        try {
+            if (!mediaBlobUrl) {
+                console.error('No media blob URL available');
+            }
+            const blob = await fetch(mediaBlobUrl!).then(res => res.blob());
+            // const correctedBlob = new Blob([blob], { type: 'audio/wav' });
+            const formData = new FormData();
+            formData.append('file', blob, 'myAudio.wav');
+            console.log('formdata', formData);
+
+            // Send audio blob to your backend
+            const response = await axios.post('http://127.0.0.1:5000/chat', formData, {
+                headers: {
+                    'Content-Type': 'audio/mpeg',
+                },
+            });
+            const responseData = response.data;
+
+            // Set the response (the transcribed text) as the input value
+            setInputValue(responseData.response || '');
+
+            setMessages([...messages, `You: ${inputValue}`, `AI: ${responseData.response}`]);
+            setIsLoading(false); // reset loading state to false
+
+        } catch (e) {
+            console.error('There was an error sending the audio file!', e);
+            setIsLoading(false); // reset loading state to false
+        }
+    };
+
 
     useEffect(() => {
         fetchDataFromServer();
@@ -53,18 +63,31 @@ const InterviewPage: React.FC = () => {
                 {messages.map((msg, idx) => (
                     <div key={idx}>{msg}</div>
                 ))}
-                {isLoading && <div>Loading...</div>} {/* Display loading when processing */}
             </div>
             <div className='flex flex-row justify-between mt-4 rounded-md space-x-4'>
-                <input 
-                    type="text" 
-                    value={inputValue} 
+                <input
+                    type="text"
+                    value={inputValue}
                     onChange={e => setInputValue(e.target.value)}
                     placeholder="Type your message..."
                     className='w-full p-4 flex-direction bg-slate-100 '
                 />
-                <button className='p-4 bg-black text-white' onClick={handleSendMessage}>Send</button>
+                {/* <button className='p-4 bg-black text-white' onClick={handleSendMessage}>Send</button> */}
             </div>
+            <ReactMediaRecorder
+                audio
+                render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+                    <div>
+                        <p>{status}</p>
+                        <button onClick={startRecording}>Start Recording</button>
+                        <button onClick={() => {
+                            stopRecording();
+                            handleStopRecording(mediaBlobUrl);
+                        }}>Stop Recording</button>
+                        <audio src={mediaBlobUrl} controls />
+                    </div>
+                )}
+            />
         </div>
     );
 };
--- server/server.py
+++ server/server.py
@@ -1,30 +1,51 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-from langchain.chat_models import ChatOpenAI   
-from flask import request, jsonify
 from dotenv import load_dotenv
 import os
+import openai
+from langchain.chat_models import ChatOpenAI
+from fastapi import HTTPException
 
 load_dotenv("../.env")
 
 openai_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = openai_key
 
 chat_model = ChatOpenAI(openai_api_key=openai_key)
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})  # This will allow all origins for all routes
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/chat', methods=['POST'])
 def chat():
     try:
-        data = request.get_json()
-        prompt = data['text']
-        print(prompt)
-        response = chat_model.predict(prompt)
-        return jsonify(response=response)
+        print(request.files)  # See if the files are actually being received.
+        print('hi')
+        if 'audio' not in request.files:
+            print('no audio found')
+            return jsonify(error="No audio file"), 400
+
+        audio_file = request.files['audio']
+        print('trying to transcribe')
+        # Transcribing the audio to text using Whisper ASR
+        try:
+            transcript = openai.Audio.transcribe(
+                "whisper-1", audio_file, api_key=openai_key)
+        except Exception as e:
+            print('error', e)
+            raise HTTPException(
+                status_code=400, detail="Could not transcribe audio")
+
+        print(transcript)
+        question = transcript["text"]
+        
+        # Getting a response for the transcribed text
+        chat_response = chat_model.predict(question)
+        
+        return jsonify(response=chat_response)
+
     except Exception as e:
         return jsonify(error=str(e)), 500
-
 
 @app.route('/')
 def hello_world():
